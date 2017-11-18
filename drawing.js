8a438c3fff9bae24fde67e9eca8c9f30da32e50d
Brent Pappas <pappasbrent@gmail.com>
2017-11-18 00:38:54 -0500

    Fix

--- drawing.js
+++ drawing.js
@@ -1,24 +1,24 @@
-var diameter = 40;
-var radius = diameter / 2;
+var radius = 20;
+var diameter = radius * 2;
 
-var noteInput = "";
+// var noteInput = "CcDFfEeGaAbB";
 
-var xPadding = radius * 2;
-var yPadding = radius * 3;
+var xPadding = radius;
+var yPadding = radius;
 
 var noteHeights = {
-    'a': radius * 2.5,
-    'A': radius * 2.5,
-    'b': radius,
-    'B': radius,
-    'C': radius * 9,
-    'c': radius * 9,
-    'D': radius * 8,
-    'E': radius * 7,
-    'e': radius * 7,
-    'F': radius * 5.5,
-    'f': radius * 5.5,
-    'G': radius * 4,
+    'a': yPadding * 3 / 2,
+    'A': yPadding * 3 / 2,
+    'b': yPadding * 0,
+    'B': yPadding * 0,
+    'C': yPadding * 18 / 2,
+    'c': yPadding * 18 / 2,
+    'D': yPadding * 15 / 2,
+    'E': yPadding * 12 / 2,
+    'e': yPadding * 12 / 2,
+    'F': yPadding * 9 / 2,
+    'f': yPadding * 9 / 2,
+    'G': yPadding * 6 / 2,
 };
 
 function circle(x, y, d) {
@@ -29,31 +29,33 @@ function draw_lines(x, y, noteRadius, numNotes) {
     fill(155, 155, 155);
     for (var i = 0; i < 3; i++) {
         x1 = x;
-        x2 = x + (numNotes * xPadding);
-        y1 = y + (i * yPadding);
+        x2 = x + (numNotes * xPadding) + (numNotes * noteRadius);
+        y1 = y + (i * (yPadding + noteRadius));
         y2 = y1;
         line(x1, y1, x2, y2);
     }
 }
 
-function draw_notes(x, y, notes, noteRadius) {
+function draw_notes(x, y, notes, noteDiameter) {
+    noteRadius = noteDiameter / 2;
     numNotes = notes.length;
     for (var i = 0; i < numNotes; i++) {
         currentNote = notes[i];
-        posX = x + (i * xPadding) + noteRadius;
-        posY = y + noteHeights[currentNote] - noteRadius;
+        posX = x + (i * xPadding) + (i * noteRadius) + noteRadius;
+        posY = y + yPadding + noteHeights[currentNote] - noteRadius;
         if (currentNote == currentNote.toLowerCase()) {
             fill(255, 0, 128);
         }
         else {
             fill(0, 0, 0);
         }
-        circle(posX, posY, noteRadius);
+        circle(posX, posY, noteRadius * 2);
     }
 }
 
 function preload() {
-    noteInput = $("#note_characters").text()
+    noteInput = $("#note_characters").text();
+    // noteInput = 'CcDFfEeGaAbB';
 }
 
 function setup() {
@@ -63,10 +65,10 @@ function setup() {
 }
 
 function draw() {
-    fill(155, 155, 155);
+    fill(255, 255, 255);
     // rect(-1, -1, width + 1, height + 1);
-    stroke(155, 155, 155);
-    draw_lines(0, 100, radius, noteInput.length);
+    stroke(15, 15, 15);
+    draw_lines(0, 100, diameter, noteInput.length);
     stroke(0, 0, 0);
-    draw_notes(0, 100, noteInput, radius);
+    draw_notes(0, 100, noteInput, diameter);
 }
